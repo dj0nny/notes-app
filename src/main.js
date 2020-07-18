9b43552678292414f96abc666f4a29f1cf2d69b0
dj0nny <frank96990@gmail.com>
2020-07-18 11:22:26 +0200

    project setup, add UIkit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 import App from './App.vue';
 import router from './router';
 
+import 'uikit/dist/css/uikit.min.css';
+
+UIkit.use(Icons);
+
 Vue.config.productionTip = false;
+
+Vue.prototype.$uikit = UIkit;
 
 new Vue({
   router,
