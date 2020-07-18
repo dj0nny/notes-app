38caaf4904eb0f6de59506379d1f8448aa3ec2b4
dj0nny <frank96990@gmail.com>
2020-07-18 11:32:07 +0200

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+      <ul class="uk-navbar-nav">
+        <a href="" class="uk-navbar-item uk-logo">Notes app</a>
+        <li class="uk-active"><a href="">Add note</a></li>
+        <li class="uk-parent"><a href="">Repository</a></li>
+        <li><a href=""></a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
